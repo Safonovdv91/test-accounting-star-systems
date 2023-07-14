fae6c820f8f3dd580f18cf6ce92b3e129723a901
Safonov <softik.d@gmail.com>
2023-07-14 11:12:31 +0300

    reformat PEP8

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ app = FastAPI()
 def main():
     return mongo_database.main()
 
+
 @app.get("/stars_system/{name_star_system}")
 def stars_system(name_star_system: str):
     return output.Handler_response().get_planets_from_system(name_star_system)
 
+
 @app.get("/stars_system/")
 def stars_system():
     return output.Handler_response().get_stars_systems()
+
 
 if __name__ == "__main__":
     main()
