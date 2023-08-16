f486bcf0c0d8826bc022e10709e4528fb9095c2a
Safonov <softik.d@gmail.com>
2023-08-16 19:18:48 +0300

    lesson #3 Validation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from Model import mongo_database
 from Controller import output
@@ -10,9 +12,9 @@ from Controller import input
 app = FastAPI()
 
 fake_star_systems = [
-    {"id": 1, "name": "Solar", "mass_center": "Sun"},
-    {"id": 2, "name": "Sambuka", "mass_center": "Unknown"},
-    {"id": 3, "name": "Wood", "mass_center": "Tree"},
+    {"id": 1, "name": "Solar","age": 35_000, "mass_center": "Sun"},
+    {"id": 2, "name": "Sambuka","age": 35_000, "mass_center": "Unknown"},
+    {"id": 3, "name": "Wood","age": 35_000, "mass_center": "Tree"},
 ]
 
 fake_universe_objects = [
@@ -39,7 +41,35 @@ fake_universe_objects = [
 ]
 
 
-@app.get("/stars_system/{stars_system_id}")
+class PermittedTypesCelestialBody(Enum):
+    star = "star"
+    blackhole = "blackhole"
+    blue_gigant = "blue_gigant"
+    worm_hole = "worm_hole"
+    unknown = "unknown"
+
+
+class PermittedTypesMassCenter(Enum):
+    star = "star"
+    blackhole = "blackhole"
+
+
+class CelestialBody(BaseModel):
+    name: str
+    age: float | int = Field(ge=0)
+    type_object: PermittedTypesCelestialBody = Field()
+    diameter: int | float = Field(ge=0)
+    weight: int | float = Field(ge=0)
+    created_at: datetime
+
+
+class StarSystem(BaseModel):
+    id: int
+    name: str
+    age: int | float = Field(ge=0)
+    mass_center: Optional[CelestialBody]
+
+@app.get("/stars_system/{stars_system_id}", response_model=List[StarSystem])
 def get_star_system(stars_system_id):
     return [system for system in fake_star_systems if system.get("id") == int(stars_system_id)]
 
@@ -52,6 +82,12 @@ def rename_star_system(stars_system_id: int, name: str):
     return {"status": 200, "data": current_star_system}
 
 
-@app.get("/universe_objects/")
+@app.post("/stars_system_add")
+def add_star_system(star_system: List[StarSystem]):
+    fake_star_systems.extend(star_system)
+    return {"status": 200, "data": fake_star_systems}
+
+
+@app.get("/universe_objects/", response_model=List[CelestialBody])
 def get_universe_objects(limit: int = 3, offset: int = 0):
     return fake_universe_objects[offset:][:limit]
